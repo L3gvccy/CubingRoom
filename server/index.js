2db799e30794af5b762c450c12f70916fc62201a
L3gvccy <oleksandr_iv@knu.ua>
2025-12-19 16:15:29 +0200

    Project setup for client and server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const dbUrl = process.env.DB_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser);
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("MongoDB is ready!");
+  })
+  .catch((error) => {
+    console.log(`Error while connecting to MongoDB:\n ${error}`);
+  });
