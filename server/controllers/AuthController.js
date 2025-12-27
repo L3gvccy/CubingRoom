d72c5f8009798c18a2d91dce4556639c0279c98b
L3gvccy <oleksandr_iv@knu.ua>
2025-12-27 20:12:03 +0200

    add getMe api

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,22 @@
 import { compare } from "bcrypt";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
+
+export const getMe = async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).send("Користувача не знайдено");
+    }
+
+    return res.status(200).json({
+      user: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Помилка сервера");
+  }
+};
--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import { getMe } from "../controllers/authController.js";
 
 const authRoutes = Router();
+
+authRoutes.get("/me", verifyToken, getMe);
 
 export default authRoutes;
