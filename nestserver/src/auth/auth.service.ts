02ec0e2eeed4485b5a236d3312cddebeddf0b225
L3gvccy <oleksandr_iv@knu.ua>
2026-01-06 20:43:54 +0200

    NestJS server initialize; Initialize Prisma+Postgres; add auth resource and User model

---
+++ nestserver/src/auth/auth.service.ts
@@ -0,0 +1,61 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { PrismaService } from "src/prisma.service";
+import { AuthDto } from "./dto/auth.dto";
+import * as bcrypt from "bcrypt";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+  ) {}
+
+  async login(dto: AuthDto) {
+    const { email, password } = dto;
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new NotFoundException("Користувача з такою поштою не знайдено");
+    }
+
+    const valid = bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new UnauthorizedException("Невірний пароль");
+    }
+
+    const payload = { sub: user.id };
+    const token = this.jwtService.sign(payload);
+
+    return { user, token };
+  }
+
+  async register(dto: AuthDto) {
+    const { email, password } = dto;
+    const exists = await this.prisma.user.findUnique({ where: { email } });
+
+    if (exists) {
+      throw new BadRequestException("Користувач з такою поштою вже існує");
+    }
+
+    const hashed: string = await bcrypt.hash(password, 10);
+    const displayName = email.split("@")[0];
+
+    const user = await this.prisma.user.create({
+      data: { email, password: hashed, displayName },
+    });
+
+    const payload = { sub: user.id };
+    const token = this.jwtService.sign(payload);
+
+    return { user, token };
+  }
+}
