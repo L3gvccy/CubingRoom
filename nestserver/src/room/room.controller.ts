45ace637ab30ba0d407636ecca21e7f20895b001
L3gvccy <oleksandr_iv@knu.ua>
2026-01-08 20:37:15 +0200

    Rooms basic CRUD

--- nestserver/src/room/room.controller.ts
+++ nestserver/src/room/room.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
-import { RoomService } from './room.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from "@nestjs/common";
+import { RoomService } from "./room.service";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { CurrentUser } from "src/common/decorators/current-user.decorator";
+import { CreateRoomDto } from "./dto/create-room.dto";
 
-@Controller('room')
+@Controller("room")
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get("all")
+  async getAllRooms() {
+    return await this.roomService.getAllRooms();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get("/:roomId")
+  async getRoomById(@Param("roomId") roomId: string) {
+    return await this.roomService.getRoomById(roomId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post("create")
+  async createRoom(
+    @Body() dto: CreateRoomDto,
+    @CurrentUser() currentUser: { id: string }
+  ) {
+    return await this.roomService.createRoom(currentUser.id, dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete("delete/:roomId")
+  async deleteRoom(
+    @Body() dto: { roomId: string },
+    @CurrentUser() currentUser: { id: string }
+  ) {
+    return await this.roomService.deleteRoom(currentUser.id, dto.roomId);
+  }
 }
--- nestserver/src/room/room.service.ts
+++ nestserver/src/room/room.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { CreateRoomDto } from "./dto/create-room.dto";
 
 @Injectable()
-export class RoomService {}
+export class RoomService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAllRooms() {
+    const rooms = await this.prisma.room.findMany({ include: { users: true } });
+    return { rooms };
+  }
+
+  async getRoomById(roomId: string) {
+    const id = parseInt(roomId);
+    if (!isNaN(id)) {
+      throw new NotFoundException("Кімната не знайдена");
+    }
+
+    const room = await this.prisma.room.findUnique({ where: { id } });
+    if (!room) {
+      throw new NotFoundException("Кімната не знайдена");
+    }
+
+    const solves = await this.prisma.roomSolve.findMany({
+      where: { roomId: room.id },
+      include: {
+        scramble: true,
+        results: {
+          include: { user: { include: { user: true } }, result: true },
+        },
+      },
+    });
+
+    return { room, solves };
+  }
+
+  async createRoom(userId: string, dto: CreateRoomDto) {
+    const room = await this.prisma.room.create({
+      data: {
+        ownerId: userId,
+        event: "333",
+        name: dto.name,
+        private: dto.isPrivate,
+        password: dto.isPrivate ? dto.password : null,
+      },
+    });
+
+    return { room };
+  }
+
+  async deleteRoom(userId: string, roomId: string) {
+    const id = parseInt(roomId);
+    if (!isNaN(id)) {
+      throw new NotFoundException("Кімната не знайдена");
+    }
+
+    const room = await this.prisma.room.findUnique({ where: { id } });
+    if (room?.ownerId !== userId) {
+      throw new BadRequestException("Видалити кімнату може лише власник");
+    }
+
+    await this.prisma.room.delete({ where: { id } });
+
+    return { message: "Кімната була успішно видалена" };
+  }
+}
