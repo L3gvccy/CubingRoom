d8f7424fcf0c35e29b8383ee62ad8e9f2e432209
L3gvccy <oleksandr_iv@knu.ua>
2026-01-24 18:45:32 +0200

    redirect update

--- client/src/pages/auth/components/wca-id-btn.jsx
+++ client/src/pages/auth/components/wca-id-btn.jsx
@@ -7,7 +7,7 @@ const WcaIdBtn = () => {
     <div
       className="flex gap-2 bg-zinc-800 hover:bg-zinc-700 active:bg-zinc-900 text-zinc-100 dark:bg-zinc-200 dark:hover:bg-zinc-100 dark:active:bg-zinc-300 dark:text-zinc-900 items-center justify-center py-2 rounded-md cursor-pointer"
       onClick={() => {
-        window.location.href = `${process.env.VITE_SERVER_URL}/api/wca/auth`;
+        window.location.href = `${import.meta.env.VITE_SERVER_URL}/api/wca/auth`;
       }}
     >
       <img src="WCALogo.png" alt="wca logo" height="20px" width="20px" />
